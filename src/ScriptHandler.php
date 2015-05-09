2d4a8f6d36cfac17c84cc37e8ffbd8398e1cd0b6
Allan Chappell <generalredneck@gmail.com>
2015-05-08 22:50:51 -0500

    Automatically use copy mode if symlinks are impossible.

--- src/ScriptHandler.php
+++ src/ScriptHandler.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tangler;
 
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOException;
 use Composer\Script\Event;
 
 class ScriptHandler
@@ -14,6 +16,18 @@ class ScriptHandler
         $project = isset($config['project']) ? $config['project'] : getcwd();
         $drupal = isset($config['drupal']) ? $config['drupal'] : getcwd() . DIRECTORY_SEPARATOR . 'www';
         $copy = isset($config['copy']);
+        if (!$copy) {
+            // Check for symlink ability. If we don't have it, hard copy.
+            // This is great for VMs on Windows hosts.
+            $fs = new Filesystem();
+            try {
+                $fs->symlink($drupal, 'test');
+                $fs->remove('test');
+            }
+            catch (IOException $e) {
+                $copy = TRUE;
+            }
+        }
         $mapper = new Mapper($project, $drupal, $copy);
         $mapper->mirror($mapper->getMap(
             $composer->getInstallationManager(),
