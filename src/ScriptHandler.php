85832fe348d61f27c52400efc17351267c6209dc
Allan Chappell <generalredneck@gmail.com>
2015-05-08 21:33:55 -0500

    adding the ability to add options using extra.

--- src/ScriptHandler.php
+++ src/ScriptHandler.php
@@ -9,7 +9,12 @@ class ScriptHandler
     public static function postUpdate(Event $event)
     {
         $composer = $event->getComposer();
-        $mapper = new Mapper(getcwd(), getcwd().'/www');
+        $extra = $composer->getPackage()->getExtra();
+        $config = isset($extra['tangler']) ? $extra['tangler'] : array();
+        $project = isset($config['project']) ? $config['project'] : getcwd();
+        $drupal = isset($config['drupal']) ? $config['drupal'] : getcwd().'/www';
+        $copy = isset($config['copy']);
+        $mapper = new Mapper($project, $drupal, $copy);
         $mapper->mirror($mapper->getMap(
             $composer->getInstallationManager(),
             $composer->getRepositoryManager()
